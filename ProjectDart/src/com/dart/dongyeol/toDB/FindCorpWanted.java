1ed4d0cd06dc8dd2223d06dca767379e12fb3f3c
qoehd4 <qoehd4@gmail.com>
2023-03-08 17:54:00 +0900

    2023-03-08 17:53

--- ProjectDart/src/com/dart/dongyeol/toDB/FindCorpWanted.java
+++ ProjectDart/src/com/dart/dongyeol/toDB/FindCorpWanted.java
@@ -1,8 +1,15 @@
 package com.dart.dongyeol.toDB;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -11,34 +18,69 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 //API KEY:182023b0d0bb93439f2cdd9f8f8ed93215fb72dd
 public class FindCorpWanted {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		List<String> corpName_corpCode = findCodeName(); //총개수 3542
 		System.out.println(corpName_corpCode.get(3540)); //엠에프엠코리아-01384477
+		String base_url = "https://opendart.fss.or.kr/api/company.json?crtfc_key=182023b0d0bb93439f2cdd9f8f8ed93215fb72dd&corp_code=name";
 		
-		System.out.println(corpName_corpCode.stream());
+		List<String> codes = corpName_corpCode.stream().map(s -> s.substring(s.indexOf("?")+1)).toList();
+		List<String> requestUrls = codes.stream().map(s->base_url.replace("name", s)).toList();
 		
-//		String base_url = "https://opendart.fss.or.kr/api/company.json?crtfc_key=182023b0d0bb93439f2cdd9f8f8ed93215fb72dd&corp_code=name";
-//		int count=0;
-//		
-//		for(String name_code:corpName_corpCode) {
-//			//회사 고유번호를 찾기 위한 코드
-//			int beginindex = name_code.indexOf("?") + 1;
-//			String corpCode = name_code.substring(beginindex);
-//			String requestUrl=base_url.replace("name", corpCode);
-//			
-//			
-//			
-//			
-//			count++;
-//			
-//		}
-//		
-//		System.out.println("------------------");
-//		System.out.println(count);
+		System.out.println(requestUrls);
+		
+		List<String> responses = new ArrayList<>();
+		
+		for(String url:requestUrls) {
+			HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
+			BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+			StringBuffer sb = new  StringBuffer();
+			String inputline = "";
+			
+			while((inputline=bf.readLine()) != null) sb.append(inputline);
+			bf.close();
+			String response = sb.toString();
+			
+			responses.add(response);
+			Thread.sleep(200);
+		}
+		
+		
+		writeExcel((ArrayList<String>) responses);
+	
+		
+		
+
 	}
 	
 	
 	
+	public static void writeExcel(ArrayList<String> list) {
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet("기업개황 json");
+		
+		int rowNo = 0;
+		
+		for(String corpJson:list) {
+			XSSFRow row = sheet.createRow(rowNo);
+			row.createCell(0).setCellValue(corpJson);
+			rowNo++;
+		}
+		
+		try(FileOutputStream fileOut = new FileOutputStream(new File("./src/com/dart/dongyeol/toDB/corpJson.xlsx"));) {
+			workbook.write(fileOut);
+			workbook.close();
+		} catch (Exception e) {
+			System.out.println("exel 데이터를 저장하는데 문제가 발생했습니다.");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+
+
 	public static List<String> findCodeName() {
 		List<String> corpName_corpCode = new ArrayList<>();
 		
